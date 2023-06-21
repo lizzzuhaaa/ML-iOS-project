1a41b88dad778646e428eee3069dad4decef2389
DonnieMix <kyrylo.derkach@ukma.edu.ua>
2023-06-22 00:42:28 +0300

    initial commit

---
+++ ML-Derkach-Sydorova/ML-Derkach-Sydorova/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ML-Derkach-Sydorova
+//
+//  Created by Kyrylo Derkach on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
